2035a02490f184b665163d7056b6579cf0550544
Jean-François Eudeline <jfeudeline@gmail.com>
2024-10-25 12:05:02 +0200

    Fin du tuto

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Customers",
+};
+
 export default async function Page() {
   await new Promise((resolve) => setTimeout(resolve, 3000));
-    return <p>Customers Page</p>;
-  }
+  return <p>Customers Page</p>;
+}
